057af5bbf8f726c1aeac18bdfc53cbbaf7d27e63
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-04-11 18:34:13 +0400

    js lessons enter

---
+++ JSLessons/Lesson1-intro/js/main.js
@@ -0,0 +1,17 @@
+// debugger
+// document.write("Hello1");
+// debugger
+// console.log("Hello1");
+// console.loga("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+// console.log("Hello6");
+// console.log("Hello7");
+// console.log("Hello8");
+// console.log("Hello9");
+// console.log("Hello10");
+// console.log("Hello11");
+
+prompt("Emailinizi daxil edin");
+// alert("Abune olursuz?");
