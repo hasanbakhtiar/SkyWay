c507e2986e12a641c7aa865afa0a4af1c421914b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-04-14 19:29:42 +0400

    operators

---
+++ JSLessons/Lesson2-dataTypes/js/main.js
@@ -0,0 +1,95 @@
+// // ALL DATA TYPES
+// //Primitive Data Types (Besit data tipleri)
+// // String dt
+// console.log("Hello"+"JS");
+
+// // Number
+// console.log(5+10);
+
+// // Boolean
+
+// console.log(true);
+// console.log(false);
+
+
+// // ======================================================
+// // Composite Data Types (Birleshik data tipleri)
+
+// // Array dt
+// var a = ['table',20,true,"pen"];
+// console.log(a);
+
+// // Object dt
+
+// var b = {
+//     name: "Ayten",
+//     age:21
+// }
+// console.log(b);
+
+// // ======================================================
+// // Special Data Types (Xususi data tipleri)
+// // undefined
+// var c = undefined;
+// console.log(c);
+
+// // null
+// var e = null;
+// console.log(e);
+
+
+// OPERATORS
+
+// Assignment operators (Daxil etme ve ya menimsetme operatorlari)
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+// var a=5;
+// a**=10;
+// console.log(a);
+
+// Aricmatic operators (Riyazi operators)
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// var a = 5;
+// var b = 10;
+// console.log(a%b);
+
+
+// Compare operators
+// ==
+// ===
+// >
+// >=
+// <
+// <=
+// !=
+// !==
+
+var a = "5";//string
+var b = 5;//number
+
+if (a!==b) {
+  console.log(true);  
+}else{
+    console.log(false);
+}
+
+
+
+
+
+
+
+
