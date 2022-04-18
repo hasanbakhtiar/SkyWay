d6f59a407e64370cd18c479a824049ccddf572fd
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-04-18 18:53:50 +0400

    end operators

--- JSLessons/Lesson2-dataTypes/js/main.js
+++ JSLessons/Lesson2-dataTypes/js/main.js
@@ -11,7 +11,6 @@
 // console.log(true);
 // console.log(false);
 
-
 // // ======================================================
 // // Composite Data Types (Birleshik data tipleri)
 
@@ -36,7 +35,6 @@
 // // null
 // var e = null;
 // console.log(e);
-
 
 // OPERATORS
 
@@ -66,7 +64,6 @@
 // var b = 10;
 // console.log(a%b);
 
-
 // Compare operators
 // ==
 // ===
@@ -77,19 +74,121 @@
 // !=
 // !==
 
-var a = "5";//string
-var b = 5;//number
+// var a = "5";//string
+// var b = 5;//number
 
-if (a!==b) {
-  console.log(true);  
-}else{
-    console.log(false);
+// if (a!==b) {
+//   console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+// if (!false) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// var enterSign = "nicat@gmail.com";
+// var enterPass = "abc123";
+
+// var defaultSign = "nicat@gmail.com";
+// var defaultPass = "abc123";
+
+// if ((enterPass === defaultPass) && (enterSign === defaultSign) ) {
+//   console.log('sign in success');
+// }else{
+//   console.log('login or password wrong');
+// }
+
+// String operators
+// +
+// +=
+
+// var a = "JavaScript";
+// var b = "Hello ";
+// a+=b;
+// console.log(a);
+
+// Special Operators
+// ,
+
+// var a = 5,
+//   b = 10;
+
+// \
+// var a = 'Nicat\'s book';
+// var a = "Nicat\"s book";
+// console.log(a);
+
+// shert ?duz netice:sehv netice
+// 5 > 10 ? console.log("duzdu") : console.log("sehvdir");
+
+// typeof
+// console.log(typeof(5));
+
+// new
+// this
+class Car {
+  constructor(model) {
+    this.brand = model;
+    return this.brand;
+  }
 }
 
+var myCar = new Car("BMW");
+console.log(myCar instanceof Car);
 
+// delete
+var car = {
+  brand: "BMW",
+  model: "X5",
+};
+// // in
+// delete car.brand;
+// console.log(car.brand);
+console.log("brand" in car);
 
+// if       - eger
+// else     - deyilse
+// else if  - dyilse eger
 
+// var a = 1;
 
+// if(a == 5){
+//   console.log('this is 5');
+// }else if(a == 6){
+//   console.log('this is 6');
+// }else if(a == 7){
+//   console.log('this is 7');
+// }else{
+//   console.log('something wrong');
+// }
 
+var myTime = 7;
 
-
+if (myTime >= 5 && myTime <= 10) {
+  console.log("Good morning");
+  if (myTime == 5) {
+    console.log("get up");
+  } else if (myTime == 7) {
+    console.log("have breakfast");
+  } else if (myTime == 10) {
+    console.log("go to work");
+  }
+} else if (myTime > 10 && myTime <= 17) {
+  console.log("Good afternoon");
+} else if (myTime > 17 && myTime <= 21) {
+  console.log("Good evening");
+} else if (myTime >= 22 && myTime < 23) {
+  console.log("Good night");
+} else if (myTime == 23) {
+  console.log("hey man, go to rest");
+} else {
+  console.log("no time");
+}
