8b398ed0a07399af6551da125c6585e08ba1e3fc
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-04-22 10:39:57 +0400

    switch

---
+++ JSLessons/Lesson3-variablesSwitchTry/js/main.js
@@ -0,0 +1,88 @@
+// ecmaScript
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+// 9.Eyni dəyişkən adını dəişkən ifadələrində istifadə etmək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var      -> variable      -> global
+// let      -> let           -> local
+// const    -> constant      -> local
+
+// var a = "Hello";
+//  a = "new";
+// console.log(a);
+
+// console.log(b);
+// var b = 10;
+//global
+// if(true){
+//     if (true) {
+//         const a = 5;
+//         console.log(a);
+
+//     }
+// }
+
+// const a = 'hello';
+
+// switch (a) {
+//   case 5:
+//     console.log("a 5dir");
+//     break;
+//   case "hello":
+//     console.log("a hellodur");
+//     break;
+//   default:
+//     console.log("err");
+// }
+
+
+
+
+// const info = 5;
+
+// switch(info){
+//  case 5:
+//      if (info <10) 
+//          console.log('hello 10');
+//  break;
+//  default:
+//      const err = "404 not found";
+//      console.log(err);    
+// }    
+
+// try {
+//     throw 'bu bir istisnadir'
+//     console.loag('new info');
+// } catch (err) {
+//     console.log(err);
+//     // console.log('texniki xeta var');
+// }finally{
+//     console.log('men her iki halda ishleyirem');
+// }
+
+
+
+// 
---
+++ JSLessons/Lesson4-Loops/js/main.js
@@ -0,0 +1,22 @@
+// LOOPS  -> Donguler
+// while
+// 1shert  2netice 3hadise
+
+// let a = 10;
+// while(a>=0){
+//     console.log(a);
+//     a--;
+// }
+
+// do while
+//  1netice 2hadise 3shert
+// let a = 0;
+// do {
+//   console.log(a);
+//   a++;
+// } while (a > 10);
+
+
+// for(let a = 0; a<10; a++){
+//     console.log(a);
+// }
